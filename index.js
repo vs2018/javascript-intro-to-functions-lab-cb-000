01354c7a7b33242b7bd40893d1491913a5f7d143
Vishal Sharma <vsh1819@gmail.com>
2018-08-03 06:12:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,12 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() == string) {
+    return "I can't hear you!"
+  }
+  
+}
+
+
