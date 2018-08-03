0ab18bafce9a1adaeda4f17461a5ba101f7201cb
Vishal Sharma <vsh1819@gmail.com>
2018-08-03 06:13:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function sayHiToGrandma(string) {
   } else if (string.toUpperCase() == string) {
     return "YES INDEED!"
   } else {
-    "I love you, too."
+    return "I love you, too."
   }
 
 }
